26d73a286e9b31806506c63d54206aa48b9c3c2c
Prakash Sharma <parkashjajra106@gmail.com>
2025-12-08 18:08:19 +0530

    Update allDataSlice.js

--- src/reduxtool/allDataSlice.js
+++ src/reduxtool/allDataSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchAllData = createAsyncThunk("allData/fetch", async () => {
-  const res = await axios.get("https://backend11-ar3o.onrender.com/api/all");
+  const res = await axios.get("https://backend-232.onrender.com/api/all");
   console.log(res)
   return res.data
 });
